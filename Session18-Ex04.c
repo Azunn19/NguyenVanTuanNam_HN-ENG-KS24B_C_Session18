36f93f119720e4ad221891e78b8b99880f60803d
Nguyễn Văn Tuấn Nam <nn1307634@gmail.com>
2024-12-23 21:44:39 +0700

    Add files via upload

---
+++ Session18-Ex04.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<string.h>
+struct sinhVien{
+	char name[50];
+	char phoneNumber[50];
+	int id;
+	int age;
+};
+
+int main(){
+    struct sinhVien sv[50];
+	for (int i = 0;i < 5;i++){
+		sv[i].id = i + 1;
+		printf("Hay nhap thong tin sinh vien thu %d:\n",i + 1);
+		fflush(stdin);
+		printf("Hay nhap ten: ");
+		fgets(sv[i].name,sizeof(sv),stdin);
+		sv[i].name[strlen(sv[i].name) - 1] = '\0';
+		printf("Hay nhap so dien thoai: ");
+		fgets(sv[i].phoneNumber,sizeof(sv),stdin);
+		sv[i].phoneNumber[strlen(sv[i].phoneNumber) - 1] = '\0';
+		printf("Hay nhap tuoi: ");
+		scanf("%d",&sv[i].age);
+    }  
+    for (int i = 0;i < 5;i++){
+    	printf("\nSinh vien thu %d:\n",i + 1);
+    	printf("Id: %d\n",sv[i].id);
+    	printf("Ten: %s\n",sv[i].name);
+    	printf("Tuoi: %d\n",sv[i].age);
+    	printf("So dien thoai: %s\n",sv[i].phoneNumber);
+	}
+    
+	return 0;
+}
+
